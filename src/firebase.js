d13e687c17ec208b0443830f708f3796e4a8ec8e
Suchita Ghadge <suchita8494.sg@gmail.com>
2021-04-19 07:29:06 -0700

    initial commit: Created login with google feature

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCaoLyqRRC0Z-DH0I9de09c3ri2GsvfM4U",
+    authDomain: "myinsta-46948.firebaseapp.com",
+    projectId: "myinsta-46948",
+    storageBucket: "myinsta-46948.appspot.com",
+    messagingSenderId: "59955659138",
+    appId: "1:59955659138:web:4c0003bab3cfb3e4418f9b"
+  };
+  // Initialize Firebase
+ const firebaseApp =  firebase.initializeApp(firebaseConfig);
+
+ const db = firebaseApp.firestore();
+ const auth = firebase.auth();
+ const storage = firebase.storage();
+ const provider = new firebase.auth.GoogleAuthProvider();
+
+
+ export { db, auth, provider, storage };
